c20f08602f7ffe14af724471602258223969fe67
Orangy <chinguyen280200@gmail.com>
2024-11-15 13:02:11 +0700

    our-service: sửa và chỉnh lại giao diện

--- js/app.js
+++ js/app.js
@@ -1,14 +1,13 @@
 import products from "./data.js";
 // product
+const categories = document.querySelectorAll(".category__title");
 
-function selectItem(element) {
-  // Loại bỏ class 'active' từ các mục khác
-  const items = document.querySelectorAll(".product__menu--title");
-  items.forEach((item) => item.classList.remove("under--action"));
-
-  // Thêm class 'active' vào mục được click
-  element.classList.add("under--action");
-}
+categories.forEach((category) => {
+  category.addEventListener("click", () => {
+    categories.forEach((cat) => cat.classList.remove("selected"));
+    category.classList.add("selected");
+  });
+});
 
 // hàm sản phẩm 6 ảnh
 
@@ -87,7 +86,42 @@ listCategories.forEach((item) => {
         <img class="product__image" src="${product.image}" alt="">
         <div class="category">${product.category}</div>
         <div class="product__name">${product.product__name}</div>
-        <div class="product__price"> ${product.product__price}</div>
+        <div class="product__price__all"> 
+        ${
+          product.product__price
+            ? `<div class="product__price"> ${product.product__price}</div>`
+            : ""
+        }
+       ${
+         product.product__price__new
+           ? ` <div class="product__price__new"> ${product.product__price__new}</div>`
+           : ""
+       }
+        ${
+          product.product__price__old
+            ? ` <div class="product__price__old"> ${product.product__price__old}</div>`
+            : ""
+        }
+      
+      </div>
+      <div class="voucher">
+        ${
+          product.product__selling
+            ? `<p class="voucher__sell">${product.product__selling}</p>`
+            : ""
+        }
+        ${
+          product.product__voucher
+            ? `<p class="product__voucher" >${product.product__voucher}</p>`
+            : ""
+        }
+
+        ${
+          product.product__new
+            ? `<p class="voucher__new">${product.product__new}</p>`
+            : ""
+        }
+      </div>
       </div>
     `
       )
